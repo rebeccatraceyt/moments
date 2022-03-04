3e0b77a0e2e8fcb737be3ce1419189b0ef9b64f7
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2022-03-04 20:09:50 +0000

    Create NavBar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import Button from 'react-bootstrap/Button';
+import NavBar from './components/NavBar';
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <Button variant="primary">Primary</Button>
+            <NavBar/>
         </div>
     );
 }
