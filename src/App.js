1e9eb8f7de72443e330061e12848591b9da8ef58
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2022-03-09 14:56:53 +0000

    Create Routing for navigation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import NavBar from './components/NavBar';
-import styles from './App.module.css'
-
+import styles from './App.module.css';
+import Container from 'react-bootstrap/Container';
+// Import Switch and Route components to set up routing.
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
     return (
         <div className={styles.App}>
             <NavBar/>
+            <Container className={styles.Main}>
+                <Switch>
+                    {/* 
+                    - Switch holds all the Routes, and renders a given component when a Route path matches the current URL.
+                    - The rendered prop on our Route component accepts a funtion that returns a component to be rendered when
+                      the Route path is matched.
+                    - The path prop is the browser URL the user will be at when they see the component in our render prop.
+                    - The 'exact' prop tells the route to only render its component when the url entered is exactly the same. 
+                    */}
+                    <Route exact path='/' render={()=> <h1>Home Page</h1>}/>
+                    <Route exact path='/signin' render={()=> <h1>Sign In</h1>}/>
+                    <Route exact path='/signup' render={()=> <h1>Sign Up</h1>}/>
+                    <Route render={()=> <p>Oops! Page not found.</p>}/>
+                </Switch>
+            </Container>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+// Router imported here to maintain tidiness (wrapping the App component inside)
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Router>
+			{/* In order for Router to work, the App component needs to be wrapped inside */}
+    		<App />
+		</Router>
+    </React.StrictMode>,
+
+	// Refers to the 'root' div in index.html
+  	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
