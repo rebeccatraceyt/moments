7d07618d942dd8e93e9e564b758b33d96abe6a48
rebeccatraceyt <rebecca.traceytimoney@gmail.com>
2022-04-26 15:31:27 +0000

    Add edit  profile functionality

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Dropdown from 'react-bootstrap/Dropdown';
+import { useHistory } from "react-router-dom";
 import styles from "../styles/MoreDropdown.module.css";
 
 // ThreeDots function needs to be passed a reference (ref) to the dropdown.toggle component
@@ -42,3 +43,34 @@ export const MoreDropdown = ({ handleEdit, handleDelete }) => {
         </Dropdown>
     );
 };
+
+export function ProfileEditDropdown({ id }) {
+    const history = useHistory();
+    return (
+        <Dropdown className={`ml-auto px-3 ${styles.Absolute}`} drop="left">
+            <Dropdown.Toggle as={ThreeDots} />
+            <Dropdown.Menu>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit`)}
+                    aria-label="edit-profile"
+                >
+                    <i className="fas fa-edit" /> edit profile
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit/username`)}
+                    aria-label="edit-username"
+                >
+                    <i className="far fa-id-card" />
+                    change username
+                </Dropdown.Item>
+                <Dropdown.Item
+                    onClick={() => history.push(`/profiles/${id}/edit/password`)}
+                    aria-label="edit-password"
+                >
+                    <i className="fas fa-key" />
+                    change password
+                </Dropdown.Item>
+            </Dropdown.Menu>
+        </Dropdown>
+    );
+}
--- src/pages/profiles/ProfilePage.js
+++ src/pages/profiles/ProfilePage.js
@@ -17,6 +17,7 @@ import { useProfileData, useSetProfileData } from "../../contexts/ProfileDataCon
 import InfiniteScroll from "react-infinite-scroll-component";
 import Post from "../posts/Post";
 import { fetchMoreData } from "../../utils";
+import { ProfileEditDropdown } from "../../components/MoreDropdown";
 
 function ProfilePage() {
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -75,6 +76,7 @@ function ProfilePage() {
 
     const mainProfile = (
         <>
+            {profile?.is_owner && <ProfileEditDropdown id={profile?.id} />}
             <Row noGutters className="px-3 text-center">
                 <Col lg={3} className="text-lg-left">
                     <Image
